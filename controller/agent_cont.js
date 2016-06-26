3700c02dfa88d555cd9e43b49dfe1aaa96b40f9e
daniarherikurniawan <ddhhkk2@gmail.com>
2016-06-26 19:57:39 +0700

    update backend

--- controller/agent_cont.js
+++ controller/agent_cont.js
@@ -10,7 +10,6 @@ var User = require('../dbhelper/user_model');
 var HostServer = require('../private_data/agent_files/hostServer');
 
 var host = HostServer.name;
-// var host = "http://localhost:8000";
 
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
