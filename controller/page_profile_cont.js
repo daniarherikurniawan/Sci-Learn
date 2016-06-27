882246d6fc6f9200011188e5385986056e772a6d
daniarherikurniawan <ddhhkk2@gmail.com>
2016-06-28 00:50:06 +0700

    update backend

--- controller/page_profile_cont.js
+++ controller/page_profile_cont.js
@@ -92,104 +92,104 @@ module.exports = {
 
 
  	showPostsByActivity: function(req, res, userId, page, limit, typeActivity ){
-		User.model.findById(userId, function(err, user){
-			showPost = false;
-			showComment = false;
-			showLike = false;
-			showShare = false;
-			if(err){
-				console.log(err);
-				res.sendStatus('404');
-			}else{
-				req.session.dataCurrentProfile = user;
-				limit = parseInt(limit);
-				arrayPostId = new Array();
-				switch(typeActivity){
-					case('post'):
-						showPost = true; urlActivity = "user/post"
-						arrayPostId = req.session.dataCurrentProfile.id_user_posts.slice();
-						break;
-					case('like'):
-						showLike = true; urlActivity = "user/like"
-						arrayPostId = req.session.dataCurrentProfile.id_liked_posts.slice();
-						break;
-					case('share'):
-						showShare = true; urlActivity = "user/share"
-						arrayPostId = req.session.dataCurrentProfile.id_share_posts.slice();
-						break;
-					case('comment'):
-						showComment = true; urlActivity = "user/comment"
-						arrayId = new Array();
-						arrayPostId = req.session.dataCurrentProfile.id_commented_posts.slice();
-						for (var i = arrayPostId.length - 1; i >= 0; i--) {
-							arrayId.push(arrayPostId[i].id);
-						};
-						arrayPostId = arrayId;
-						break;
-				}
-				console.log(showPost+" typeActivity : "+typeActivity)
-
-				Post.model
-					.find({'_id': {$in : arrayPostId}})
-					.skip(limit*page)
-					.limit(limit)
-					.sort({date_created: 'desc'})
-					.populate({
-						  path: 'creator',
-						})
-					.populate({
-						  path: 'original_creator',
-						})
-					.populate('post_shared')
-					.exec(function (err,posts){
-						id = req.session.profile._id;
-						if(posts != null )
-							for (var i = posts.length - 1;  i >= 0; i--) {
-
-								if(posts[i].creator._id == req.session.profile._id){
-									 posts[i].creator = null;
-								}
-								if(posts[i].like.indexOf(id) != -1){
-									 posts[i].liked = true;
-								}
-								if(posts[i].share.indexOf(id) != -1){
-									posts[i].shared = true;
-								} 
-								if((posts[i].creator == null) && (posts[i].post_shared != null)  && (posts[i].post_shared.share.indexOf(id) != -1)){
-									posts[i].post_shared.shared = true;
-								}
+			User.model.findById(userId, function(err, user){
+				// console.log(req.session.profile)
+				showPost = false;
+				showComment = false;
+				showLike = false;
+				showShare = false;
+				if(err){
+					console.log(err);
+					res.sendStatus('404');
+				}else{
+					req.session.dataCurrentProfile = user;
+					limit = parseInt(limit);
+					arrayPostId = new Array();
+					switch(typeActivity){
+						case('post'):
+							showPost = true; urlActivity = "user/post"
+							arrayPostId = req.session.dataCurrentProfile.id_user_posts.slice();
+							break;
+						case('like'):
+							showLike = true; urlActivity = "user/like"
+							arrayPostId = req.session.dataCurrentProfile.id_liked_posts.slice();
+							break;
+						case('share'):
+							showShare = true; urlActivity = "user/share"
+							arrayPostId = req.session.dataCurrentProfile.id_share_posts.slice();
+							break;
+						case('comment'):
+							showComment = true; urlActivity = "user/comment"
+							arrayId = new Array();
+							arrayPostId = req.session.dataCurrentProfile.id_commented_posts.slice();
+							for (var i = arrayPostId.length - 1; i >= 0; i--) {
+								arrayId.push(arrayPostId[i].id);
 							};
-					
-						var idForProfile = req.session.profile.connections.slice();
-						idForProfile.push(req.session.profile._id);
-						numOfPost = (page*limit)+posts.length;
-						numOfLastPage = Math.ceil(arrayPostId.length/limit);
+							arrayPostId = arrayId;
+							break;
+					}
+					console.log(showPost+" typeActivity : "+typeActivity)
 
-						if (idForProfile.indexOf(""+req.session.dataCurrentProfile._id) != -1){
-							// profile owner 
-					 	res.render('profile', {profile: req.session.profile,
-					 		 rec_topic : req.session.rec_topic, showPost : showPost,  urlActivity: urlActivity,
-					 		 showComment : showComment, showLike : showLike, showShare : showShare,
-					 		friendProfile: null, numOfPost:numOfPost,
-					 		posts: posts, myFriend : true, numOfCurrPage : page, page : true,
-					 		popular_topic: req.session.popular_topic, showPerActivity : true,
-					 		numOfLastPage : numOfLastPage, limitPerPage:limit,
-					 	partials: { rightSide:'rightSide', topNavigation:'topNavigation'}});	
+					Post.model
+						.find({'_id': {$in : arrayPostId}})
+						.skip(limit*page)
+						.limit(limit)
+						.sort({date_created: 'desc'})
+						.populate({
+							  path: 'creator',
+							})
+						.populate({
+							  path: 'original_creator',
+							})
+						.populate('post_shared')
+						.exec(function (err,posts){
+							id = req.session.profile._id;
+							if(posts != null )
+								for (var i = posts.length - 1;  i >= 0; i--) {
 
-						}else{
-					 	res.render('profile', {profile: req.session.profile, numOfCurrPage : page, showPost : showPost, 
-							showLike : showLike, showShare : showShare, showComment : showComment,
-					 		numOfLastPage : numOfLastPage,  limitPerPage:limit,  numOfPost:numOfPost,
-					 		friendProfile: req.session.dataCurrentProfile, page : true, urlActivity: urlActivity,
-					 		popular_topic: req.session.popular_topic, showPerActivity : true,
-					 		posts: posts, myFriend :  false,  rec_topic : req.session.rec_topic,
-					 	partials: { rightSide:'rightSide', topNavigation:'topNavigation'}});	
+									if(posts[i].creator._id == req.session.profile._id){
+										 posts[i].creator = null;
+									}
+									if(posts[i].like.indexOf(id) != -1){
+										 posts[i].liked = true;
+									}
+									if(posts[i].share.indexOf(id) != -1){
+										posts[i].shared = true;
+									} 
+									if((posts[i].creator == null) && (posts[i].post_shared != null)  && (posts[i].post_shared.share.indexOf(id) != -1)){
+										posts[i].post_shared.shared = true;
+									}
+								};
+						
+							var idForProfile = req.session.profile.connections.slice();
+							idForProfile.push(req.session.profile._id);
+							numOfPost = (page*limit)+posts.length;
+							numOfLastPage = Math.ceil(arrayPostId.length/limit);
 
-						}
-					
-				});
-			}
-		});		
+							if (idForProfile.indexOf(""+req.session.dataCurrentProfile._id) != -1){
+						 	res.render('profile', {profile: req.session.profile,
+						 		 rec_topic : req.session.rec_topic, showPost : showPost,  urlActivity: urlActivity,
+						 		 showComment : showComment, showLike : showLike, showShare : showShare,
+						 		friendProfile: req.session.dataCurrentProfile, numOfPost:numOfPost,
+						 		posts: posts, myFriend : true, numOfCurrPage : page, page : true,
+						 		popular_topic: req.session.popular_topic, showPerActivity : true,
+						 		numOfLastPage : numOfLastPage, limitPerPage:limit,
+						 	partials: { rightSide:'rightSide', topNavigation:'topNavigation'}});	
+
+							}else{
+						 	res.render('profile', {profile: req.session.profile, numOfCurrPage : page, showPost : showPost, 
+								showLike : showLike, showShare : showShare, showComment : showComment,
+						 		numOfLastPage : numOfLastPage,  limitPerPage:limit,  numOfPost:numOfPost,
+						 		friendProfile: req.session.dataCurrentProfile, page : true, urlActivity: urlActivity,
+						 		popular_topic: req.session.popular_topic, showPerActivity : true,
+						 		posts: posts, myFriend :  false,  rec_topic : req.session.rec_topic,
+						 	partials: { rightSide:'rightSide', topNavigation:'topNavigation'}});	
+
+							}
+						
+					});
+				}
+			});			
 	}
 
 }
--- routes/user_router.js
+++ routes/user_router.js
@@ -1,31 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-var multiparty = require('multiparty');
-var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart();
-var fs = require('fs');
-var Client = require('node-rest-client').Client;
-var async = require('async');
-var requestify = require('requestify'); 
-var client = new Client();
-var rimraf = require('rimraf');
 
-/*Controller*/
-var agent_cont = require('../controller/agent_cont');
-var api_cont = require('../controller/api_cont');
-var chat_cont = require('../controller/chat_cont');
-var connection_cont = require('../controller/connection_cont');
-var group_cont = require('../controller/group_cont');
-var post_cont = require('../controller/post_cont');
-var tutorial_cont = require('../controller/tutorial_cont');
-var user_cont = require('../controller/user_cont');
-
+var page_profile_cont = require('../controller/page_profile_cont');
 
 /* GET home page. */
 router.get('/post', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'post')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'post')
 	}else{
 		res.redirect('/login');
 	}
@@ -33,7 +14,7 @@ router.get('/post', function(req, res, next) {
 
 router.get('/post/:page', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'post')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'post')
 	}else{
 		res.redirect('/login');
 	}
@@ -41,7 +22,7 @@ router.get('/post/:page', function(req, res, next) {
 
 router.get('/post/:page/:limit', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'post')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'post')
 	}else{
 		res.redirect('/login');
 	}
@@ -50,7 +31,7 @@ router.get('/post/:page/:limit', function(req, res, next) {
 /* GET home page. */
 router.get('/share', function(req, res, next) {
 	if(req.session.profile!=null){	
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'share')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'share')
 	}else{
 		res.redirect('/login');
 	}
@@ -58,7 +39,7 @@ router.get('/share', function(req, res, next) {
 
 router.get('/share/:page', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'share')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'share')
 	}else{
 		res.redirect('/login');
 	}
@@ -66,7 +47,7 @@ router.get('/share/:page', function(req, res, next) {
 
 router.get('/share/:page/:limit', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'share')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'share')
 	}else{
 		res.redirect('/login');
 	}
@@ -74,7 +55,7 @@ router.get('/share/:page/:limit', function(req, res, next) {
 /* GET home page. */
 router.get('/like', function(req, res, next) {
 	if(req.session.profile!=null){	
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'like')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'like')
 	}else{
 		res.redirect('/login');
 	}
@@ -82,7 +63,7 @@ router.get('/like', function(req, res, next) {
 
 router.get('/like/:page', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'like')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'like')
 	}else{
 		res.redirect('/login');
 	}
@@ -90,7 +71,7 @@ router.get('/like/:page', function(req, res, next) {
 
 router.get('/like/:page/:limit', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'like')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'like')
 	}else{
 		res.redirect('/login');
 	}
@@ -98,7 +79,7 @@ router.get('/like/:page/:limit', function(req, res, next) {
 /* GET home page. */
 router.get('/comment', function(req, res, next) {
 	if(req.session.profile!=null){	
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'comment')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, 0, 15 ,'comment')
 	}else{
 		res.redirect('/login');
 	}
@@ -106,7 +87,7 @@ router.get('/comment', function(req, res, next) {
 
 router.get('/comment/:page', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'comment')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, 15 ,'comment')
 	}else{
 		res.redirect('/login');
 	}
@@ -114,116 +95,10 @@ router.get('/comment/:page', function(req, res, next) {
 
 router.get('/comment/:page/:limit', function(req, res, next) {
 	if(req.session.profile!=null){	// res.send("post")
-		showPostsPage(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'comment')
+		page_profile_cont.showPostsByActivity(req, res, req.session.dataCurrentProfile._id, req.params.page, req.params.limit ,'comment')
 	}else{
 		res.redirect('/login');
 	}
 });
-
-function showPostsPage(req, res, userId, page, limit, typeActivity ){
-	if(req.session.profile!=null){
-		mongoose.model('User').findById(userId, function(err, user){
-			// console.log(req.session.profile)
-			showPost = false;
-			showComment = false;
-			showLike = false;
-			showShare = false;
-			if(err){
-				console.log(err);
-				res.sendStatus('404');
-			}else{
-				req.session.dataCurrentProfile = user;
-				limit = parseInt(limit);
-				arrayPostId = new Array();
-				switch(typeActivity){
-					case('post'):
-						showPost = true; urlActivity = "user/post"
-						arrayPostId = req.session.dataCurrentProfile.id_user_posts.slice();
-						break;
-					case('like'):
-						showLike = true; urlActivity = "user/like"
-						arrayPostId = req.session.dataCurrentProfile.id_liked_posts.slice();
-						break;
-					case('share'):
-						showShare = true; urlActivity = "user/share"
-						arrayPostId = req.session.dataCurrentProfile.id_share_posts.slice();
-						break;
-					case('comment'):
-						showComment = true; urlActivity = "user/comment"
-						arrayId = new Array();
-						arrayPostId = req.session.dataCurrentProfile.id_commented_posts.slice();
-						for (var i = arrayPostId.length - 1; i >= 0; i--) {
-							arrayId.push(arrayPostId[i].id);
-						};
-						arrayPostId = arrayId;
-						break;
-				}
-				console.log(showPost+" typeActivity : "+typeActivity)
-
-				mongoose.model('Post')
-					.find({'_id': {$in : arrayPostId}})
-					.skip(limit*page)
-					.limit(limit)
-					.sort({date_created: 'desc'})
-					.populate({
-						  path: 'creator',
-						})
-					.populate({
-						  path: 'original_creator',
-						})
-					.populate('post_shared')
-					.exec(function (err,posts){
-						id = req.session.profile._id;
-						if(posts != null )
-							for (var i = posts.length - 1;  i >= 0; i--) {
-
-								if(posts[i].creator._id == req.session.profile._id){
-									 posts[i].creator = null;
-								}
-								if(posts[i].like.indexOf(id) != -1){
-									 posts[i].liked = true;
-								}
-								if(posts[i].share.indexOf(id) != -1){
-									posts[i].shared = true;
-								} 
-								if((posts[i].creator == null) && (posts[i].post_shared != null)  && (posts[i].post_shared.share.indexOf(id) != -1)){
-									posts[i].post_shared.shared = true;
-								}
-							};
-					
-						var idForProfile = req.session.profile.connections.slice();
-						idForProfile.push(req.session.profile._id);
-						numOfPost = (page*limit)+posts.length;
-						numOfLastPage = Math.ceil(arrayPostId.length/limit);
-
-						if (idForProfile.indexOf(""+req.session.dataCurrentProfile._id) != -1){
-							// profile owner 
-					 	res.render('profile', {profile: req.session.profile,
-					 		 rec_topic : req.session.rec_topic, showPost : showPost,  urlActivity: urlActivity,
-					 		 showComment : showComment, showLike : showLike, showShare : showShare,
-					 		friendProfile: null, numOfPost:numOfPost,
-					 		posts: posts, myFriend : true, numOfCurrPage : page, page : true,
-					 		popular_topic: req.session.popular_topic, showPerActivity : true,
-					 		numOfLastPage : numOfLastPage, limitPerPage:limit,
-					 	partials: { rightSide:'rightSide', topNavigation:'topNavigation'}});	
-
-						}else{
-					 	res.render('profile', {profile: req.session.profile, numOfCurrPage : page, showPost : showPost, 
-							showLike : showLike, showShare : showShare, showComment : showComment,
-					 		numOfLastPage : numOfLastPage,  limitPerPage:limit,  numOfPost:numOfPost,
-					 		friendProfile: req.session.dataCurrentProfile, page : true, urlActivity: urlActivity,
-					 		popular_topic: req.session.popular_topic, showPerActivity : true,
-					 		posts: posts, myFriend :  false,  rec_topic : req.session.rec_topic,
-					 	partials: { rightSide:'rightSide', topNavigation:'topNavigation'}});	
-
-						}
-					
-				});
-			}
-		});			
-	}else{
-		res.redirect('/login');
-	}
-}
 
 module.exports = router;
