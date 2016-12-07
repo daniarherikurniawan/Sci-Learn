06e42534b0e4857213f5773b68b9e0a79348ecad
daniarherikurniawan <ddhhkk2@gmail.com>
2016-12-07 20:59:58 +0700

    update live search

--- public/javascripts/page_scripting/top_navigation/top_navigation.js
+++ public/javascripts/page_scripting/top_navigation/top_navigation.js
@@ -1,14 +1,15 @@
 $('input#search-connection').on('focusout', function(e) {
 	var delay=1000; //1 second
 	setTimeout(function() {
-		$('div#search-connection').removeClass('dropdown open');
+		// $('div#search-connection').removeClass('dropdown open');
 	}, delay);
 });
 $('input#search-connection').on('keyup', function(e) {
     var http = new XMLHttpRequest();
     http.open("POST", "/connections/searchNewConnection", true);
     http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    var params = "search_term=" + $('input#search-connection').val();
+    search_term = $('input#search-connection').val();
+    var params = "search_term=" + search_term;
     http.send((params));
     http.onload = function() {
     	var search_result = JSON.parse(http.responseText);
@@ -17,10 +18,14 @@ $('input#search-connection').on('keyup', function(e) {
     	}else{
 			$('div#search-connection').addClass('dropdown open');
 	    	html_search_result = '';
-	    	for ( i = 0;i <=search_result.length - 1; i++) {
+	    	for ( i = 0;i <=search_result.length - 2; i++) {
 	    		html_search_result += 
 	    			"<li><a  href=\"/profile/"+search_result[i].email+"\">"+search_result[i].name+"</a></li>"
 	          };
+	          if(search_result.length == 8){
+	          	html_search_result += 
+	    			"<li style=\"text-align:center\"><a style=\"margin-top: 4px;    padding-left: 17px; \" href=\"/search?search_term="+search_term+"\">See all result</a></li>"
+	          }
 	        document.getElementById('search-connection-result').innerHTML =html_search_result;
     	}
     }
