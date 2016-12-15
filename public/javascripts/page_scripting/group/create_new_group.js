5afb06b76a0a0704781858d5d0c04e0366b3b4c9
daniarherikurniawan <ddhhkk2@gmail.com>
2016-12-15 20:20:29 +0700

    update UI and group membership

--- public/javascripts/page_scripting/group/create_new_group.js
+++ public/javascripts/page_scripting/group/create_new_group.js
@@ -13,20 +13,25 @@ function createNewGroup(){
 	for (var i = list_new_member.length - 1; i >= 0; i--) {
 		group_members.push(new Object(list_new_member[i]._id));
 	}
-  var http = new XMLHttpRequest();
-  http.open("POST", "/group/createNewGroup", true);
-  http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-  var params = "group_name=" + group_name+"&group_info=" + group_info+"&group_admin="+group_admin+
-  "&group_members="+group_members+"&group_accessibility="+group_accessibility;
-  // alert()
-  http.send(params);
-  http.onload = function() {
-    var result = JSON.parse(http.responseText);
-  	$('input#close-modal-create-new-group').click();
-    if(result.status == 1){
-      	window.location.href= "/group/"+result.message.group_id;
-  	}else{
-  		alert("failed create group")
-  	}
+
+  if(group_name =='' || group_info =='' || group_members.length == 1){
+        alert("Failed create group. Please fill all input columns and make sure you add another member!")
+  }else{
+    var http = new XMLHttpRequest();
+    http.open("POST", "/group/createNewGroup", true);
+    http.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    var params = "group_name=" + group_name+"&group_info=" + group_info+"&group_admin="+group_admin+
+    "&group_members="+group_members+"&group_accessibility="+group_accessibility;
+    // alert()
+    http.send(params);
+    http.onload = function() {
+      var result = JSON.parse(http.responseText);
+    	$('input#close-modal-create-new-group').click();
+      if(result.status == 1){
+        	window.location.href= "/group/"+result.message.group_id;
+    	}else{
+    		alert("failed create group")
+    	}
+    }
   }
 }
